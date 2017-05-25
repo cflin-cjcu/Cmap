2d7fb1796a853821364fa33282f135d33f8a6a44
cflin <cflin@mail.cjcu.edu.tw>
2017-05-25 16:41:47 +0800

    Initial commit

---
+++ app/src/main/java/cv/yktn/en/cmap/BasicMapActivity.java
@@ -0,0 +1,12 @@
+package cv.yktn.en.cmap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BasicMapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_map);
+    }
+}
